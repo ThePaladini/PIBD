0496165b8901d61198de17ad1fb42cf608a7b40d
ThePaladini <othepaladini@gmail.com>
2023-09-05 15:43:29 -0300

    teas de inserção

--- Pibd/Pibd/urls.py
+++ Pibd/Pibd/urls.py
@@ -6,4 +6,7 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', views.hello_world, name='hello_world'),
+    path('insert_info/', views.insert, name='insert_info'),
+    path('cadastro_ies/', views.cadastro_ies, name='cadastro_ies'),
+    path('registro_coordenador_administrativo/', views.registro_coordenador_administrativo, name='registro_coordenador_administrativo'),
 ]
--- Pibd/Pibd/views.py
+++ Pibd/Pibd/views.py
@@ -3,3 +3,12 @@ from django.http import HttpResponse
 
 def hello_world(request):
     return render(request, 'hello.html')
+
+def insert(request):
+    return render(request, 'alter_info_professor_ministrante.html')    
+
+def cadastro_ies(request):
+    return render(request, 'cadastro_ies.html')
+
+def registro_coordenador_administrativo(request):
+    return render(request, 'registro_coordenador_administrativo.html')
